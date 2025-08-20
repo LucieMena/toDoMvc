a231ec66f50dcace0fb1177b09c512aea5d25742
LuluMena <lucie.menage@gmail.com>
2025-08-20 23:21:02 +0200

    Implement ToDo application with exception handling and CRUD operations

--- src/test/java/com/example/todo/controller/ToDoControllerTest.java
+++ src/test/java/com/example/todo/controller/ToDoControllerTest.java
@@ -6,16 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
-import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
